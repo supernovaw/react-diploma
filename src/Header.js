6bccf89134a2559c9b1454e8e3585fcab3289625
supernovaw <ruszrj@gmail.com>
2022-07-29 13:46:20 -0700

    minor fix

--- src/Header.js
+++ src/Header.js
@@ -13,7 +13,7 @@ export default () => {
         </NavLink>
         <div className="collapase navbar-collapse" id="navbarMain">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active"><NavLink className="nav-link" to="/">Главная</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/">Главная</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" to="/catalog.html">Каталог</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" to="/about.html">О магазине</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" to="/contacts.html">Контакты</NavLink></li>
